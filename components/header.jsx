fe7acdc7fafccf68426eea761c4ef86c63fda05b
Vithushan <63534975+ACuteWoof@users.noreply.github.com>
2022-12-24 15:06:09 +0000

    logos

--- components/header.jsx
+++ components/header.jsx
@@ -1,22 +1,33 @@
+import { motion } from "framer-motion";
+
 function Header() {
   return (
     <header>
       <nav class='px-4 lg:px-6 py-2 pt-12'>
-        <div class='flex items-center mx-auto max-w-screen-xl'>
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          class='flex items-center mx-auto max-w-screen-xl'
+        >
           <picture className='hidden dark:block'>
             <img
-              src='iconDark.svg'
+              src='/iconDark.svg'
               class='mr-3 h-6 sm:h-9'
               alt='Ajez Media Logo'
             />
           </picture>
-          <picture className='hidden dark:hidden'>
-            <img src='icon.svg' class='mr-3 h-6 sm:h-9' alt='Ajez Media Logo' />
+          <picture className='block dark:hidden'>
+            <img
+              src='/icon.svg'
+              class='mr-3 h-6 sm:h-9'
+              alt='Ajez Media Logo'
+            />
           </picture>
           <span class='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
             Ajez Media - See You At Profit.
           </span>
-        </div>
+        </motion.div>
       </nav>
     </header>
   );
